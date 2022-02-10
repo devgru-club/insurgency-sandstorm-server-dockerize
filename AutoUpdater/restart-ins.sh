eb7658463200f23a636cbb35c54c4d51b2404681
R. Neff <29211141+diveyez@users.noreply.github.com>
2022-02-09 19:31:53 -0800

    Taking Notes

--- AutoUpdater/restart-ins.sh
+++ AutoUpdater/restart-ins.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 docker stop sandstorm-modmap
 docker rm sandstorm-modmap
+# docker pull devgru.club's autobuild and autoupdate optimized docker here
 docker pull andrewmhub/insurgency-sandstorm
 docker run -d --restart always --env-file /home/user/coop-modmap/modmap.env \
   --name sandstorm-modmap --net=host \
   -v /home/user/coop-modmap/Mods:/home/steam/steamcmd/sandstorm/Insurgency/Mods:rw \
   -v /home/user/coop-modmap/config/ini:/home/steam/steamcmd/sandstorm/Insurgency/Saved/Config/LinuxServer:ro \
-  -v /home/user/coop-modmap/config/txt:/home/steam/steamcmd/sandstorm/Insurgency/Config/Server:ro andrewmhub/insurgency-sandstorm:latest
+  -v /home/user/coop-modmap/config/txt:/home/steam/steamcmd/sandstorm/Insurgency/Config/Server:ro andrewmhub/insurgency-sandstorm:latest # need to pull autobuild here after testing as well
